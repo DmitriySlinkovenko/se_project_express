c266a03d4802691f1b6c7e2de7c7f8d44c36c163
Dmitriy <dmitriy.slinkovenko@gmail.com>
2024-06-07 18:56:54 -0400

    Project 15, added a crash test to app.js

--- app.js
+++ app.js
@@ -16,11 +16,11 @@ const indexRouter = require("./routes/index");
 app.use(express.json());
 app.use(cors());
 app.use(requestLogger);
-/*app.get("/crash-test", () => {
+app.get("/crash-test", () => {
   setTimeout(() => {
     throw new Error("Server will crash now");
   }, 0);
-});*/
+});
 app.use("/", indexRouter);
 app.use(errorLogger);
 app.use(errors());
